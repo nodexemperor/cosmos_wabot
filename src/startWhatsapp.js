70dad3983a704782880a98ba4dabfec6755f9e60
Salman Wahib <sxlmnwb.dev@gmail.com>
2023-07-30 04:23:01 +0000

    reason: make reconnect after disconnect with ... poor signal service or weak server connection (not necessarily)
    - add puppeteer args
    
    Signed-off-by: Salman Wahib <sxlmnwb.dev@gmail.com>

--- src/startWhatsapp.js
+++ src/startWhatsapp.js
@@ -3,15 +3,19 @@ const qrcode = require('qrcode-terminal');
 const useHelpCommand = require('./useHelpCommand');
 
 module.exports = function startWhatsapp() {
-    const client = new Client();
+    const client = new Client({
+        puppeteer: {
+            args: ['--no-sandbox --disable-setuid-sandbox']
+        }
+    });
 
     client.on('qr', async (qr) => {
-        console.log('QR Code received, scan please.');
+        console.log('QR Code received, scan please ...');
         qrcode.generate(qr, {small: true});
     });
 
     client.on('ready', async () => {
-        console.log('Whatsapp client bot are ready !!!');
+        console.log('cosmos_wabot client is ready and connected !!!');
         const groupId = process.env.GROUP_ID;
         const text = useHelpCommand();
         if (groupId) {
@@ -19,6 +23,15 @@ module.exports = function startWhatsapp() {
         } else {
             console.log('No GROUP_ID set in .env');
         }
+    });
+
+    client.on('disconnected', (reason) => {
+        console.log('cosmos_wabot was disconnected, reason: ', reason);
+        client.initialize();
+    });
+
+    client.on('connected', () => {
+        console.log('cosmos_wabot are connected !!!');
     });
     
     return client;
